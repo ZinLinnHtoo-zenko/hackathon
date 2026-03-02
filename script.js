6f813ef64a6b2481a8707c8b3e05e0ae11ec2a79
Zenko <131528659+ZinLinnHtoo-zenko@users.noreply.github.com>
2026-03-02 10:02:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,16 +13,22 @@ function calculateStress() {
     }
 
     let resultText = "";
+    let tips = "";
 
     if(total <= 12){
         resultText = "Low Stress";
+        tips = "Great Job! You seem to be managing well. Keep maintaining your work-life balance and stay active.";
     } else if(total <= 20){
         resultText = "Moderate Stress";
+        tips = "You're feeling some pressure. Try to prioritize your tasks, take short breaks every hour, and ensure you're getting enough sleep.";
     } else {
         resultText = "High Stress";
+        tips = "It sounds like you're overwhelmed. Consider talking to a mentor, practicing deep breathing exercises, or breaking your big projects into tiny, managable steps.";
     }
 
     document.getElementById("result").innerText = "Your Score:" + total + "->" + resultText;
+
+    document.getElementById("tips-box").innerText = tips;
 
     localStorage.setItem("Last Score", total);
 }
